66aedd564c6675a9f1f0dd61d535d01b5fa79a3f
Kyle Isom <coder@kyleisom.net>
2011-05-27 22:07:47 -0600

    update wget to show number of bytes downloaded
    TODO: please use chunking and some kind of progress bar :/

--- src/wget.ml
+++ src/wget.ml
@@ -3,9 +3,10 @@ open Unix
 
 let download = fun ~url ~filename ->
   let page = http_get url in
+  let downsz = String.length page in 
   let fd = openfile filename [ O_RDWR; O_TRUNC; O_CREAT ] 0o644 in
-  let written = single_write fd page 0 (String.length page) in
-  Printf.printf "url %s written to %s (%d bytes)\n" url filename written ;
+  let written = write fd page 0 downsz in
+  Printf.printf "url %s written to %s (%d / %d bytes)\n" url filename written downsz ;
   close fd ;;
 
 download ~url:Sys.argv.(1) ~filename:Sys.argv.(2)
