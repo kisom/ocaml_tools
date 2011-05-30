b96187f286d087f884f337ca5220b45459ea33fe
Kyle Isom <coder@kyleisom.net>
2011-05-30 12:11:24 -0600

    finish wc

--- src/wc.ml
+++ src/wc.ml
@@ -10,7 +10,7 @@ type result = { mutable characters:int; mutable words:int; mutable lines:int }
 let max_read = 16
 
 let display_result = fun res filename ->
-  Printf.printf "%d\t%d\t%d %s\n" res.characters res.words res.lines filename
+  Printf.printf "%d\t%d\t%d %s\n" res.lines res.words res.characters filename
 
 let add_result = fun r1 r2 ->
   { characters = (r1.characters + r2.characters);
@@ -33,17 +33,17 @@ let scan = fun line ->
 ;;
 
 let scanfd = fun ?(filename = "") fd ->
-  let buf = String.make max_read ' ' in 
-  let res = { characters = 0; words = 0; lines = 0 } in
+  let buf = String.make max_read ' ' in
+  let res = ref { characters = 0; words = 0; lines = 0 } in
   let rec loop = fun () ->
   try
     let readsz = Unix.read fd buf 0 max_read in
+    let rdbuf = String.sub buf 0 readsz in 
     if readsz > 0 then begin
-      display_result (add_result res (scan buf)) "scanfd" ;
-      res = (add_result res (scan buf));
+      res := add_result !res (scan rdbuf);
       loop ();
     end
-    else display_result res filename
+    else display_result !res filename
   with 
     | _ -> ()
   in loop ()
@@ -54,6 +54,21 @@ let main = fun () ->
   let len = Array.length Sys.argv in
   if len = 1 then
     scanfd Unix.stdin
+  else
+    let rec loop = fun n ->
+      if n >= Array.length Sys.argv then ()
+      else
+	try 
+	  let fd = Unix.openfile Sys.argv.(n) [ Unix.O_RDONLY; ] 0 in
+	  let _ = scanfd ~filename:Sys.argv.(n) fd in 
+	  let _ = Unix.close fd in
+	  loop (n + 1)
+        with
+	  | Unix.Unix_error (Unix.ENOENT, "open", filename) ->
+	    prerr_string (Filename.basename Sys.argv.(0) ^ ": " ^ filename ^ 
+			    ": No such file or directory\n") ;
+	    loop (n + 1);
+    in loop 1
 ;;
 
 main ()
